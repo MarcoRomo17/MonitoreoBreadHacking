b8ef0b293de707ec50127b9f77bd3d7e33d9ad8b
VictorManuelAlvarezMayorga <utm23090718@utma.edu.mx>
2024-10-18 10:37:51 -0600

    Cambios front

--- src/MonitoreoBH_frontend/src/main.jsx
+++ src/MonitoreoBH_frontend/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.scss';
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import {Reports} from './Reports';
 import { ReportForm } from './ReportFrom';
 import { Marco } from './Marco';
 
